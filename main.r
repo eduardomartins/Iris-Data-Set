8985778b5b233cead6bc7c20efceb10aea61793a
Eduardo Sant' Anna Martins <eduardo@eduardomartins.site>
2019-01-12 17:31:34 -0200

    ADD SCATTER CURVE

--- main.r
+++ main.r
@@ -6,23 +6,72 @@
 ##################################################
 
 
-dataset.test <- read.csv("dataset/iris.data", sep=",", header=F)
 
-colnames(dataset.test) <- c("sepal.length", "sepal.width", "petal.length", "petal.width", "class")
+main <- function(){
+  
+  dataset.test <- read.csv("dataset/iris.data", sep=",", header=F)
+  
+  colnames(dataset.test) <- c("sepal.length", "sepal.width", "petal.length", "petal.width", "class")
+  
+  folder <- "plots"
+  
+  for (class.name in c("Iris-versicolor", "Iris-setosa", "Iris-virginica")){
+    
+    dataset.iris <- dataset.test[dataset.test$class == class.name, ]
+  
+    x <- dataset.iris$sepal.length
+  
+    y <- dataset.iris$sepal.width
+    
+    f.name <- file.path(folder, paste("scatter-", class.name,  ".pdf", sep = "")) 
+  
+    myscatter.plot(x, y, file.name=f.name, m="Dispertion Graphic", x.lab="Sepal Length", y.lab="Sepal Width")
+    
+    #f.name <- file.path(folder, paste("bell-sepal-length", class.name,  ".pdf", sep = "")) 
+    
+    #mybell.plot(x, file.name=f.name, m="Bell curve Graphic", x.lab="Sepal Length")
+    
+    #f.name <- file.path(folder, paste("bell-sepal-width", class.name,  ".pdf", sep = "")) 
+    
+    #mybell.plot(y, file.name=f.name, m="Bell Curve Graphic", x.lab="Sepal width")
+  
+    
+  }
+  
+}
 
-summary(dataset.test)
 
-den <- density(dataset.test$`sepal.width`)
+myscatter.plot <- function(x, y, file.name = "plot.pdf", m = NA, x.lab = NA, y.lab = NA){
+  
+  #print(paste(x, y, file.name, m, x.lab, y.lab, sep = ", "))
+  
+  pdf(file.name)
+  
+  plot(x, y, main=m, xlab=x.lab, ylab=y.lab, pch=19)
+  
+  abline(lm(x~y), col="red")
+  
+  lines(lowess(x, y), col="blue")
+  
+  dev.off()
+  
+}
 
-plot(den, main="Iris Plants Database")
 
-polygon(den, col="red", border="blue")
+mybell.plot <- function(value, file.name = "plot.pdf", m = NA, x.lab = NA){
+  
+  #print(paste(x, y, file.name, m, x.lab, y.lab, sep = ", "))
+  
+  pdf(file.name)
+  
+  hist(value, col = "red", freq = F, xlim = 2 + c(min(value), max(value)))
+  
+  curve( dnorm(value, mean = mean(value), sd = 2), min(value), max(value), add = T, col="blue")
+  
+  dev.off()
+  
+}
 
-#hist(dataset$`sepal length`, col = "red")
 
-#hist(dataset$`sepal width`, col = "red")
-
-#hist(dataset$`petal length`, col = "red")
-
-#hist(dataset$`petal width`, col = "red")
+print(main())
 
